7494f055cdb2e2f2aa1f6e3c081922af9d3080cf
Zeynep Yavuz <zeynepyavuz2001@hotmail.com>
2024-07-19 15:21:49 +0300

    import .scss files

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -1,9 +1,13 @@
 // js files
 import { handleSubmit } from './js/formHandler'
-
-
+import { checkForName } from './js/nameChecker'
 
 // alert("I EXIST")
 // console.log("CHANGE!!");
 
 // sass files
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
