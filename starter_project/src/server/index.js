4061d36de3bb9c63338aeadac613be95de1459e3
Zeynep Yavuz <zeynepyavuz2001@hotmail.com>
2024-08-02 01:04:29 +0300

    fetch api

--- starter_project/src/server/index.js
+++ starter_project/src/server/index.js
@@ -4,26 +4,46 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const app = express();
+const baseUrl = "https://api.meaningcloud.com/sentiment-2.1";
+const apiKey = process.env.API_KEY;
 
 const cors = require('cors');
 
+const app = express();
+
+app.use(express.static('dist'))
 app.use(cors());
 app.use(bodyParser.json());
 
 console.log(__dirname);
 
-// Variables for url and api key
-
-
 app.get('/', function (req, res) {
-    res.send("This is the server API page, you may access its services via the client app.");
+    res.sendFile('dist/index.html')
 });
 
+// POST ROUTE
 
-// POST Route
+app.post("/api", async function (req, res) {
+  const analisysDataRes = await callMeaningCloud(req.body.url);
+  console.log(analisysDataRes);
+  res.send(analisysDataRes);
+});
 
+async function callMeaningCloud(blogUrl) {
+  const formData = new FormData();
+  formData.append("key", apiKey);
+  formData.append("language", "en");
+  formData.append("url", blogUrl);
+  let payload = {
+    method: "POST",
+    body: formData,
+    redirect: "follow",
+  };
 
+  const data = await fetch(baseUrl, payload);
+  const response = await data.json();
+  return response;
+}
 
 // Designates what port the app will listen to for incoming requests
 app.listen(8000, function () {
